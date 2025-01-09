7ad0e7b12745ee79973d700cbc005ec921950804
Euna Kwon <eunakwon@naver.com>
2025-01-09 16:33:04 +0900

    style: 임의대로 탭 설정해놨던 것 원래대로 돌려놓음.

--- calenderBasketball/ThundersCalender.Designer.cs
+++ calenderBasketball/ThundersCalender.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.calender = new System.Windows.Forms.TableLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -95,8 +95,8 @@
             this.calender.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.2851F));
             this.calender.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.2851F));
             this.calender.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28939F));
-            this.calender.Controls.Add(this.button2, 1, 1);
             this.calender.Controls.Add(this.button1, 0, 1);
+            this.calender.Controls.Add(this.button2, 1, 1);
             this.calender.Controls.Add(this.button3, 2, 1);
             this.calender.Controls.Add(this.button4, 3, 1);
             this.calender.Controls.Add(this.button5, 4, 1);
@@ -158,16 +158,6 @@
             this.calender.Size = new System.Drawing.Size(461, 197);
             this.calender.TabIndex = 0;
             // 
-            // button2
-            // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(68, 33);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(59, 21);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -177,6 +167,17 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(68, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+
             // 
             // button3
             // 

ea574aeba9c344915038bb784370b7e921b959c5
euna kwon <eunakwon@naver.com>
2025-01-10 00:05:18 +0900

    feat: 기능구현을 위한 GameDTO 생성

---
+++ samsungT/DTO/GameDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samsungT.DTO
+{
+    internal class GameDTO
+    {
+        public int GameID { get; set; }
+        public DateTime Date { get; set; }
+        public int HomeTeamID { get; set; }
+        public int AwayTeamID { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+    }
+}

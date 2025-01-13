a9148abca728a59a70dde5bcb2f15d0c4dde4a07
Euna Kwon <eunakwon@naver.com>
2025-01-13 10:17:57 +0900

    feat: 3점, 야투 확률 계산 후 차트 제작

--- samsungT/Views/thundersChart.cs
+++ samsungT/Views/thundersChart.cs
@@ -28,8 +28,6 @@ namespace samsungT.Views
             Chart_Thunders.Series[0].Points.Clear();
 
             List<Game> games = db.GetGames();
-
-            DateTime[] dateTimes = new DateTime[games.Count];
             int score;
 
             foreach (Game game in games)
@@ -46,20 +44,89 @@ namespace samsungT.Views
 
         private void Button_3Point_Click(object sender, EventArgs e)
         {
-            //var playerStatus = db.GetPlayersStatus();
-            //Chart_Thunders.Series[0].Points.Clear();
+            Chart_Thunders.Series[0].Points.Clear();
+            var playerStatus = db.GetPlayersStatus();
+            List<Game> games = db.GetGames();
+            int[] gameIDs = new int[games.Count];
+            for (int i = 0; i < games.Count; i++)
+            {
+                gameIDs[i] = db.GetSearchGame(games[i].Date).GameID;
+            }
+            Dictionary<DateTime, (int total3Point, int total3PointA)> dateStatus = new Dictionary<DateTime, (int, int)>();
 
-            //List<Game> games = db.GetGames();
+            foreach (var status in playerStatus)
+            {
+                for (int i = 0; i < gameIDs.Length; i++)
+                {
+                    if (status.GameID == gameIDs[i])
+                    {
+                        if (!dateStatus.ContainsKey(games[i].Date))
+                        {
+                            dateStatus[games[i].Date] = (0, 0);
+                        }
 
-            //DateTime[] dateTimes = new DateTime[games.Count];
-            //int score;
+                        dateStatus[games[i].Date] = (dateStatus[games[i].Date].total3Point + status.ThreePoint,
+                                                          dateStatus[games[i].Date].total3PointA + status.ThreePointA);
+                    }
+                }
+            }
+            var sortStatus = dateStatus.OrderBy(k => k.Key);
+            var filteredData = sortStatus.Where(entry => entry.Value.total3PointA > 0);
 
+            foreach (var filter in filteredData)
+            {
+                var date = filter.Key;
+                var (total3Point, total3PointA) = filter.Value;
 
+                if (total3PointA > 0)
+                {
+                    Chart_Thunders.Series[0].Points.AddXY(date, (double)total3Point/total3PointA * 100);
+                }
+            }
         }
+
 
         private void Button_Field_Click(object sender, EventArgs e)
         {
+            Chart_Thunders.Series[0].Points.Clear();
+            var playerStatus = db.GetPlayersStatus();
+            List<Game> games = db.GetGames();
+            int[] gameIDs = new int[games.Count];
+            for (int i = 0; i < games.Count; i++)
+            {
+                gameIDs[i] = db.GetSearchGame(games[i].Date).GameID;
+            }
+            Dictionary<DateTime, (int totalField, int totalFieldA)> dateStatus = new Dictionary<DateTime, (int, int)>();
 
+            foreach (var status in playerStatus)
+            {
+                for (int i = 0; i < gameIDs.Length; i++)
+                {
+                    if (status.GameID == gameIDs[i])
+                    {
+                        if (!dateStatus.ContainsKey(games[i].Date))
+                        {
+                            dateStatus[games[i].Date] = (0, 0);
+                        }
+
+                        dateStatus[games[i].Date] = (dateStatus[games[i].Date].totalField + status.FieldGoal,
+                                                          dateStatus[games[i].Date].totalFieldA + status.FieldGoalA);
+                    }
+                }
+            }
+            var sortStatus = dateStatus.OrderBy(k => k.Key);
+            var filteredData = sortStatus.Where(entry => entry.Value.totalFieldA > 0);
+
+            foreach (var filter in filteredData)
+            {
+                var date = filter.Key;
+                var (totalField, totalFieldA) = filter.Value;
+
+                if (totalFieldA > 0)
+                {
+                    Chart_Thunders.Series[0].Points.AddXY(date, (double)totalField / totalFieldA * 100);
+                }
+            }
         }
 
         private void Button_Free_Click(object sender, EventArgs e)

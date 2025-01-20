55d52bc22934b374458c467b109b79e680e53e4a
Euna Kwon <eunakwon@naver.com>
2025-01-20 16:31:39 +0900

    refactor: addTeam viewModel와 view 분리

--- samsungT/Views/addTeamForm.cs
+++ samsungT/Views/addTeamForm.cs
@@ -8,36 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using samsungT.Models;
+using samsungT.ViewModels;
 
 namespace samsungT
 {
     public partial class addTeamForm : Form
     {
+        private addTeamViewModel viewModel;
         private DatabaseHelper db;
         public addTeamForm()
         {
             InitializeComponent();
             db = new DatabaseHelper();
+            viewModel = new addTeamViewModel();
+            teamName.DataBindings.Add("Text", viewModel, "TeamName", true, DataSourceUpdateMode.OnPropertyChanged);
+            teamCity.DataBindings.Add("Text", viewModel, "City", true, DataSourceUpdateMode.OnPropertyChanged);
         }
 
         private void resister_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(teamName.Text) ||
-                string.IsNullOrWhiteSpace(teamCity.Text))
-            {
-                MessageBox.Show("모든 텍스트를 입력해주세요.", "모든 텍스트를 입력해주세요.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
             try
             {
-                Team team = new Team
-                {
-                    TeamName = teamName.Text,
-                    City = teamCity.Text
-                };
-
-                db.AddTeam(team);
+                viewModel.ResisterTeam();
                 MessageBox.Show("팀이 추가되었습니다.", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 teamName.Clear();
@@ -48,8 +40,10 @@ namespace samsungT
 
             catch (Exception ex)
             {
-                MessageBox.Show($"팀 추가 중 오류가 발생했습니다: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(ex.Message, "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+
     }
 }
